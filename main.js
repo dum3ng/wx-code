e2f54c86daa946abd91bb8b405cbd9081dc17052
dumeng <dumeng@outlook.com>
2020-05-20 15:58:36 +0800

    add

--- main.js
+++ main.js
@@ -10,15 +10,18 @@ if (s) {
   });
 }
 
+const ele = document.querySelector("code");
 if (qs.code) {
   // has code
-  const ele = document.querySelector("code");
   ele.innerText = qs.code;
 } else {
-  const appId = "wx707aad1c20975b7d";
-  const redirectUrl = encodeURIComponent(window.location.href);
-  window.location.assign(
-    `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${redirectUrl}&response_type=code&scope=snsapi_base&state=123#wechat_redirect`
-  );
-  window.location.assign();
+  ele.innerText = "no code , will redirect";
+  setTimeout(() => {
+    const appId = "wx707aad1c20975b7d";
+    const redirectUrl = encodeURIComponent(window.location.href);
+    window.location.assign(
+      `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${redirectUrl}&response_type=code&scope=snsapi_base&state=123#wechat_redirect`
+    );
+    window.location.assign();
+  }, 2000);
 }
